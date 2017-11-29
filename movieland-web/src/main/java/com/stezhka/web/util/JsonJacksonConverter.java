c548dfb7d6c776ad7572a9bcb145f7294c3b2d00
maksym <mstezhka@gmail.com>
2017-11-30 00:09:21 +0200

    fixes b-3 Get all genres

--- movieland-web/src/main/java/com/stezhka/web/util/JsonJacksonConverter.java
+++ movieland-web/src/main/java/com/stezhka/web/util/JsonJacksonConverter.java
@@ -4,18 +4,21 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stezhka.web.dto.GenreDto;
 import com.stezhka.web.dto.MovieDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class JsonJacksonConverter {
 
+    private final static Logger logger = LoggerFactory.getLogger("JsonJacksonConverter");
     private static ObjectMapper objectMapper = new ObjectMapper();
 
     public static String movieToJson(List<MovieDto> movies){
         try {
             return objectMapper.writeValueAsString(movies);
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
+            logger.debug("Serialization of a movie was aborted");
             throw new RuntimeException("Movie can't be sesialized to JSON", e);
         }
     }
@@ -24,8 +27,8 @@ public class JsonJacksonConverter {
         try {
             return objectMapper.writeValueAsString(genres);
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Movie can't be sesialized to JSON", e);
+            logger.debug("Serialization of a movie was aborted");
+            throw new RuntimeException("Genre can't be sesialized to JSON", e);
 
         }
     }
