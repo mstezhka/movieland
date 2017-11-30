d4f1060a5a3406525bcdde51acaabf4db73c8c74
maksym <mstezhka@gmail.com>
2017-11-30 16:17:53 +0200

    fixes to b3 Get_all_genres

--- movieland-web/src/main/java/com/stezhka/web/controller/GenreController.java
+++ movieland-web/src/main/java/com/stezhka/web/controller/GenreController.java
@@ -5,6 +5,8 @@ import com.stezhka.service.GenreService;
 import com.stezhka.web.dto.GenreDto;
 import com.stezhka.web.util.JsonJacksonConverter;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +18,7 @@ public class GenreController {
 
     private final GenreService genreService;
     private final ModelMapper modelMapper;
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     public GenreController(GenreService genreService, ModelMapper modelMapper) {
@@ -25,9 +28,14 @@ public class GenreController {
 
     @RequestMapping(value = "/genre", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
     public String getAll() {
+        logger.debug("Start getting all genres from service");
+        long startTime = System.currentTimeMillis();
+
         List<Genre> movies = genreService.getAll();
         List<GenreDto> genreDtos = movies.stream().map(this::genreToDto)
                 .collect(Collectors.toList());
+
+        logger.debug("End getting all genres from service. It was taken in {} ms", System.currentTimeMillis() - startTime);
         return JsonJacksonConverter.genreToJson(genreDtos);
     }
 
